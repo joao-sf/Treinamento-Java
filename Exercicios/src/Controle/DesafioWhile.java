34d43d8fbfd7d551eb9dcb9889c1d63edf447a1d
joao-sf <silva.joao.filho@gmail.com>
2023-04-22 10:56:19 -0300

    Aupdate 22/04/2023

---
+++ Exercicios/src/Controle/DesafioWhile.java
@@ -0,0 +1,41 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+	public static void main(String[] args) {
+		/*Calcular a media dos alunos de uma turma cujo a quantidade
+		   de alunos e notas na sala é indeterminado*/
+		Scanner entrada = new Scanner(System.in);
+
+		float nota = 0;
+		float media = 0;
+		float result = 0;
+		int i = 0;
+		float contador = 0;
+		//inicio
+		System.out.println("Entre com as notas do aluno, ou -1 para finalizar.");
+		nota = entrada.nextFloat();
+
+
+		while ( nota != -1) {			
+			if (nota >=0 && nota <= 10 ){					
+				nota = entrada.nextFloat();				
+
+				media = media + nota;
+				i++;
+				System.out.println("Favor entrar com a proxiam nota ou"
+						+ " entre com -1 para finalizar");
+			} else if(nota != -1){
+				System.out.println("Entre com uma nota valida");
+			}
+		}
+		result = media / i;
+		System.out.println("A media final do aluno é:" + result);	
+
+		entrada.close();
+
+
+	}
+
+}
--- Exercicios/src/Controle/For1.java
+++ Exercicios/src/Controle/For1.java
@@ -3,8 +3,12 @@ package Controle;
 public class For1 {
 
 	public static void main(String[] args) {
-		for (int contador = 0; contador<=10; contador ++){
+		for (int contador = 1; contador<=10; contador ++){
 			System.out.printf(" i=%d\n ", contador);
-		}
-		}
+		}// Exemplo clássico utilizando um, valor inicial e indo até o máximo deles
+
+		/*for (;;) {
+			System.out.println("FIM! "); //Repetição infinita.
+		}*/
 	}	
+}
